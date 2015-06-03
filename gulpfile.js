ea0dc953532c5b4395e935ad5959807540401215
Rogério Chaves <rogeriocfj@gmail.com>
2015-06-03 10:47:49 -0300

    Add srcDir option to be used by baked

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ var potato = require('potato.js');
 require('./helpers.js');
 helpers.potato = potato;
 
+baked.init({
+  options: {
+    srcDir: 'src'
+  }
+});
+
 gulp.task('serve', ['baked:serve']);
 gulp.task('default', ['baked:default']);
 gulp.task('clean', ['baked:clean']);
